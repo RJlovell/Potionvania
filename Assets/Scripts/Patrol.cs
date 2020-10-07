ffe0d0545f6dd4aeb711e82323abde1b835693a9
Ryan Alchin <azekno@hotmail.com>
2020-10-07 13:57:36 +1100

    All the things I did in the mock project. This includes the scripts for enemy movement and a barrier prefab for testing purposes

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public float maxRayDistance;
+    public float raycastOffset = 0.5f;
+
+    private bool movingRight = true;
+
+    void Update()
+    {
+        //The ray :D!
+        Ray theRay = new Ray(transform.position + new Vector3(movingRight ? raycastOffset : -raycastOffset, 0, 0), Vector3.down);
+        if (movingRight)
+        {
+            //The object is moving right
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+        }
+
+        RaycastHit groundInfo;
+
+        if (!Physics.Raycast(theRay, out groundInfo, maxRayDistance))
+        {
+            Debug.DrawLine(theRay.origin, theRay.direction, Color.blue);
+            movingRight = !movingRight;
+        }
+        else
+        {
+            Debug.DrawLine(theRay.origin, theRay.direction, Color.red);
+        }
+    }
+}
