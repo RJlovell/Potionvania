af3ff525454651391e3a25fd0187447949b4b8a8
Ryan Alchin <azekno@hotmail.com>
2020-10-08 13:28:45 +1100

    Commented out the GoblinAttackScript, pending changes. Added a script for the projectile to move towards a target.

---
+++ Assets/Scripts/Enemy/GoblinAttackScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class GoblinAttackScript : MonoBehaviour
+{
+    public Transform target;
+    //public float throwingAngle;
+    public float throwingDelay;
+    public float throwingForce;
+    //Offset to the y-axis
+    //public float throwOffset;
+    //public float artificalGravity;
+    public float timer = 0;
+    public Transform projectile;
+    private Transform myTransform;
+    //Test variables
+    public GameObject potionPrefab;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myTransform = transform;
+        //projectile = transform;
+        //projectile = myTransform;
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //if (throwingDelay == timer)
+        //{
+        //    SpawnPotion();
+        //    timer = 0;
+        //}
+        ////Short delay beford the projectile is thrown
+        ////Invoke("SpawnPotion", throwingDelay * Time.deltaTime);
+        ////Vector3 direction = target.transform.position - myTransform.position;
+        ////potionPrefab.GetComponent<Rigidbody>().AddForce(direction.normalized * throwingForce, ForceMode.Impulse);
+        //Vector3 nextPos = Vector3.MoveTowards(projectile.position, target.position, throwingForce * Time.deltaTime);
+        //projectile.rotation = Quaternion.LookRotation(nextPos - transform.position);
+        //projectile.position = nextPos;
+        //
+        //if (throwingDelay == timer)
+        //{
+        //    projectile.position = nextPos;
+        //    timer = 0;
+        //}
+        //timer++;
+        //
+        //if (nextPos == target.position)
+        //{
+        //    Arrived();
+        //}
+    }
+
+    //void Arrived()
+    //{
+    //    Debug.Log("Damage happens");
+    //}
+    //void SpawnPotion()
+    //{
+    //    Debug.Log("Spawning the potion");
+    //    GameObject spawnPotion = Instantiate(potionPrefab);
+    //    spawnPotion.gameObject.transform.position = myTransform.position;
+    //}
+}
---
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Transform targetPos;
+
+    public float arcHeight;
+    public float projectileSpeed;
+    Vector3 startPos;
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Compute the next position, with arc added in
+        float x0 = startPos.x;
+        float x1 = targetPos.position.x;
+        float distance = x1 - x0;
+        float nextX = Mathf.MoveTowards(transform.position.x, x1, projectileSpeed * Time.deltaTime);
+        float baseY = Mathf.Lerp(startPos.y, targetPos.position.y, (nextX - x0) / distance);
+        float arc = arcHeight * (nextX - x0) * (nextX - x1)/ (-0.25f * distance * distance);
+        nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
+
+        //Vector3 nextPos = Vector3.MoveTowards(transform.position, targetPos.position, projectileSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(nextPos - transform.position);
+        //transform.rotation = LookAt(nextPos - transform.position);
+        transform.position = nextPos;
+
+        if(nextPos == targetPos.position)
+        {
+            Destroy(gameObject);
+            Debug.Log("Damag Dealt");
+        }
+    }
+
+    static Quaternion LookAt(Vector3 forward)
+    {
+        return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
+    }
+}
