2b4222ed86e6b946ed65a007f098eeb7211c0349
Jack <jackrkleidon@gmail.com>
2020-10-29 13:40:59 +1100

    Testing In Scene
    put player in full grey box map. fixes to prefab.

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -145,19 +145,7 @@ public class Player : MonoBehaviour
             potionPos.x /= mag;
             potionPos.y /= mag;
 
-<<<<<<< HEAD
-            ///Boundary setting for potion throwing
-           /* if (mousePos.y < transform.position.y)
-            {
-                if (potionPos.x < 0.7 && potionPos.x > 0)
-                    potionPos.x = 0.7f;
-                if (potionPos.x > -0.7 && potionPos.x < 0)
-                    potionPos.x = -0.7f;
-                if (potionPos.y < -0.7)
-                    potionPos.y = -0.7f;
-            }*/
-=======
->>>>>>> RileysBranch
+
             potionVel = potionPos;
 
             //Debug.Log($"Normalised vec: {potionPos}");
