f2d22a7ce1987f03de9820149e5376dbe56b0ef2
Ryan Alchin <azekno@hotmail.com>
2020-11-06 16:53:08 +1100

    Updated Potion Throwing
    Added correct potion throwing script

--- Assets/Scripts/Tile/PotionThrowing.cs
+++ Assets/Scripts/Tile/PotionThrowing.cs
@@ -9,7 +9,7 @@ public class PotionThrowing : MonoBehaviour
     private Player playerScript;
     public GameObject player;
     Rigidbody rb;
-    //OrcScript orcScript;
+
 
 
     public float throwForce = 1.0f;
@@ -19,10 +19,9 @@ public class PotionThrowing : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        //potionEffect = GameObject.FindGameObjectWithTag("Potion").GetComponent<PotionEffect>();
-        //orcScript = GameObject.FindGameObjectWithTag("Orc").GetComponent<OrcScript>();
+        potionEffect = GameObject.FindGameObjectWithTag("Potion").GetComponent<PotionEffect>();
     }
-
+    
     private void FixedUpdate()
     {
         Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>(), true);
@@ -42,21 +41,9 @@ public class PotionThrowing : MonoBehaviour
         }
     }
 
-    //void OnCollisionEnter(Collision other)
-    //{
-    //    //Debug.Log("The potion dealt " + potionEffect.potionDamage + " damage //against " + other.gameObject.name);
-    //    if (other.collider.gameObject.CompareTag("Orc") || //other.collider.gameObject.CompareTag("Goblin"))
-    //    {
-    //        Debug.Log("Player has dealt " + potionEffect.potionDamage + " to the " /+ /other.gameObject.name);
-    //        if(CompareTag("Orc"))
-    //        {
-    //            //orcScript.TakeDamage(potionEffect.potionDamage);
-    //        }
-    //        if(CompareTag("Goblin"))
-    //        {
-    //
-    //        }
-    //    }
-    //    Destroy(gameObject);
-    //}
+    void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log("The potion dealt " + potionEffect.potionDamage + " damage against " + other.gameObject.name);
+        Destroy(gameObject);
+    }
 }
