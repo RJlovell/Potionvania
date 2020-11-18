ea1f892966f3671b7254353fc019fe4dc41a4bee
Riley McCarthy <rileybenjaminmcc@hotmail.com>
2020-11-19 10:56:10 +1100

    Added Animations and Animation Control Script

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            anim.SetBool("isWalking", true);
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            anim.SetBool("isWalking", false);
+    }
+}
